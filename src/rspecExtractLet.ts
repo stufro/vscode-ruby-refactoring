c563742771e2770b653a409fea1407e1e9c9782f
Stuart Frost <stuart.2.frost@bt.com>
2023-05-03 08:39:31 +0100

    Refactor rspecExtractLet() to use try/catch for error handling

--- src/rspecExtractLet.ts
+++ src/rspecExtractLet.ts
@@ -1,24 +1,26 @@
 import * as vscode from 'vscode';
 
-interface RubyVariable { name: String, content: String };
+type RubyVariable = { name: String, content: String };
 
 export default function rspecExtractLet(editor: vscode.TextEditor) {
-  editor.selection = currentLineSelection(editor);
-  const rubyVariable = findVariable(editor);
+  try {
+    editor.selection = currentLineSelection(editor);
+    const rubyVariable = findVariable(editor);
+    const blockStartPosition = editor.document.lineAt(findNearestBlock(editor, editor.selection.start));
+    const indentationLevel = calculateIndentationLevel(editor, blockStartPosition);
+    const insertPosition = blockStartPosition.range.start.with({ line: blockStartPosition.lineNumber + 1, character: indentationLevel });
 
-  if (typeof rubyVariable === 'string') {
-    vscode.window.showInformationMessage(rubyVariable);
-    return;
+    editor.edit(editBuilder => {
+      editBuilder.delete(editor.selection);
+      editBuilder.insert(insertPosition, `let(:${rubyVariable.name}) { ${rubyVariable.content} }\n${' '.repeat(indentationLevel)}`);
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      vscode.window.showInformationMessage(error.message);
+    } else {
+      vscode.window.showErrorMessage("Unknown error occurred");
+    }
   }
-
-  const blockStartPosition = editor.document.lineAt(findNearestBlock(editor, editor.selection.start));
-  const indentationLevel = calculateIndentationLevel(editor, blockStartPosition);
-  const insertPosition = blockStartPosition.range.start.with({ line: blockStartPosition.lineNumber + 1, character: indentationLevel });
-
-  editor.edit(editBuilder => {
-    editBuilder.delete(editor.selection);
-    editBuilder.insert(insertPosition, `let(:${rubyVariable.name}) { ${rubyVariable.content} }\n${' '.repeat(indentationLevel)}`);
-  });
 }
 
 function currentLineSelection(editor: vscode.TextEditor): vscode.Selection {
@@ -29,17 +31,15 @@ function currentLineSelection(editor: vscode.TextEditor): vscode.Selection {
   return new vscode.Selection(lineRange.start, lineRange.end);
 }
 
-function findVariable(editor: vscode.TextEditor): RubyVariable | string {
+function findVariable(editor: vscode.TextEditor): RubyVariable {
   const textToExtract = editor.document.getText(editor.selection);
 
-  if (textToExtract.includes("=")) {
-    const letContent = textToExtract.split("=")[1].trim();
-    const letName = textToExtract.split("=")[0].trim();
+  if (!textToExtract.includes("=")) { throw new Error("No variable found at current cursor position"); }
 
-    return { name: letName, content: letContent };
-  } else {
-    return "No variable found at current cursor position";
-  }
+  const letContent = textToExtract.split("=")[1].trim();
+  const letName = textToExtract.split("=")[0].trim();
+
+  return { name: letName, content: letContent };
 }
 
 function findNearestBlock(editor: vscode.TextEditor, position: vscode.Position): number {
